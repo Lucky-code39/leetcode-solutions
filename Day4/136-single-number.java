572478c8cac7eed662cc89fe31baf033c4183bd5
Lucky Raheja <luckyraheja19@gmail.com>
2025-11-18 01:23:10 +0530

    Added Day4 solutions

---
+++ Day4/136-single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int result = 0;
+
+        for(int element : nums){
+            result = result ^ element;
+        }
+
+        return result;
+
+        
+    }
+}
---
+++ Day4/349-intersection-of-two-arrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+       HashSet<Integer> set = new HashSet<>();
+
+       for(int element : nums1){
+        set.add(element);
+       }
+
+       HashSet<Integer> result = new HashSet<>();
+       for(int element : nums2){
+        if(set.contains(element)){
+           result.add(element);
+        }
+       }
+
+       int[] arr = new int[result.size()];
+       int i = 0;
+       for(int element : result){
+        arr[i++] = element;
+       }
+
+       return arr;
+    }
+}
