b555a148ebcf6dc1fa3eb0f1baa7a9ae615a1e16
Lucky Raheja <luckyraheja19@gmail.com>
2025-11-20 00:33:04 +0530

    added solutions for the problems no. 217,242 and 387

---
+++ Day5/217-contains-duplicate.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int element : nums){
+            if(set.contains(element)){
+                return true;
+            }
+            set.add(element);
+        }
+            
+        
+
+
+            return false;
+    }
+}
