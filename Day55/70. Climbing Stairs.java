f3c1b67d179f66891b2015f90271c1ffc57e423c
Lucky-code39 <luckyraheja19@gmail.com>
2026-04-30 23:54:00 +0530

    Add solutions to problems 70, 198 and 746

---
+++ Day55/70. Climbing Stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return dfs(n, dp);
+    }
+    private int dfs(int n, int[] dp){
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+
+          if(dp[n] != -1) return dp[n];
+        int left = dfs(n - 1, dp);
+        int right = dfs(n - 2, dp);
+
+        return dp[n] = left  + right;
+    }
+}
